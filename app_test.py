53ad9ae7e661819c6fe55117ed39d913eefdc707
dingym <dingym@yahoo.com>
2014-12-18 15:02:59 -0500

    Created app test code.

---
+++ app_test.py
@@ -0,0 +1,16 @@
+import unittest
+from app import server
+
+class OVSTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.applications = server.test_client()
+    
+    
+    def test_root(self):
+        name = 'ding'
+        result = self.applications.get('/' + name)
+        assert "Hello, " + name + "!" == result.data
+        
+if __name__ == '__main__':
+    unittest.main()
